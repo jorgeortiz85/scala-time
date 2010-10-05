0d7207f1c3ad6a38e7a49e18034bc56dc2cca423
Jorge Ortiz <jorge.ortiz@gmail.com>
2010-10-05 16:10:31 -0400

    Add sbt build system

---
+++ project/build/ScalaTimeProject.scala
@@ -0,0 +1,15 @@
+import sbt._
+ 
+class ScalaTimeProject(info: ProjectInfo) extends DefaultProject(info) with posterous.Publish {
+  override def compileOptions = Seq(Deprecation, Unchecked)
+
+  // Dependencies
+  val jodaTime = "joda-time" % "joda-time" % "1.6"
+
+  // Publishing
+  override def managedStyle = ManagedStyle.Maven
+  val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
+  Credentials(Path.userHome / ".ivy2" / ".credentials", log)
+  override def publishAction = super.publishAction && publishCurrentNotes
+  override def extraTags = "scalaj" :: super.extraTags
+}
---
+++ project/plugins/Plugins.scala
@@ -0,0 +1,5 @@
+import sbt._
+
+class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
+  val posterous = "net.databinder" % "posterous-sbt" % "0.1.6"
+}
